605d8648ea261feba5057f5b2290eac7e57f7376
N3v1 <n3v1010@gmail.com>
2025-06-14 20:53:03 +0200

    Initial Commit

---
+++ LaunchControl/LaunchControl.h
@@ -0,0 +1,16 @@
+//
+//  LaunchControl.h
+//  LaunchControl
+//
+//  Created by Nevio Hirani on 14.06.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LaunchControl.
+FOUNDATION_EXPORT double LaunchControlVersionNumber;
+
+//! Project version string for LaunchControl.
+FOUNDATION_EXPORT const unsigned char LaunchControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaunchControl/PublicHeader.h>
