5a0aafdbb2ec60eae84333b840a7ddd6010d14e9
N3v1 <n3v1010@gmail.com>
2025-06-15 04:03:05 +0200

    (dev): Add LaunchControlSimulator

---
+++ LaunchControlSimulator/LaunchControlSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchControlSimulatorApp.swift
+//  LaunchControlSimulator
+//
+//  Created by Nevio Hirani on 15.06.25.
+//
+
+import SwiftUI
+
+@main
+struct LaunchControlSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
